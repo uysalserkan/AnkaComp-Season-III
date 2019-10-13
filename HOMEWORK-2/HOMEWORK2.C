a6969877d608704d29bf1813e1247c4f523a3a29
serqkan <serqkanseko@gmail.com>
2019-10-13 12:37:47 +0300

    welcome to the new version

---
+++ HOMEWORK-2/HOMEWORK2.C
@@ -0,0 +1,12 @@
+/*
+*  ____________________________________
+* |         Comiler: GCC 7.4.0        |
+* |	    Enviroment: Ubuntu 18.04.1    |
+* | Before versionns can find multiple|
+* | words in the pattern but this not.|
+* |___________________________________|
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
