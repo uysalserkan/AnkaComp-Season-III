0c88cea0b378ba87948c0aa56d6397aecbc8ba8e
serqkan <serqkanseko@gmail.com>
2019-11-26 21:42:05 +0300

    pleasee...

--- HOMEWORK-5/Homework5.c
+++ HOMEWORK-5/Homework5.c
@@ -20,48 +20,43 @@
 */
 int nodeSize = 0;
 
-struct node
+struct treeNode
 {
-    struct node *left;
-    struct node *right;
-    int value;
-    int identity;
+	struct treeNode *left;
+	struct treeNode *right;
+	int activeHeight;
+	int value;
+	int identity;
 };
 
 struct linkedListNode
 {
-    int value;
-    int identity;
-    struct linkedListNode *next;
+	int value;
+	int identity;
+	struct linkedListNode *next;
 };
 
-struct binary_tree
-{
-    struct node *root;
-};
-
-typedef struct binary_tree BinaryTree;
-typedef struct node Node;
+typedef struct treeNode BTNode;
 typedef struct linkedListNode LLNode;
 
 // LLNode *takeDataWithLinkedList(LLNode *head);
 LLNode *takeDataUnsortedLL(LLNode *head);
 void sortTheLL(LLNode *head);
-BinaryTree *createBTree(LLNode *head);
+BTNode *createBTree(LLNode *head, int treeSize);
 void printLLNode(LLNode *head);
-int binaryTreeHeight(int BTSize);
+int BTNodeHeight(int BTSize);
 
 int main()
 {
-    LLNode *head;
-    BinaryTree *BTHead;
-    // head = (LLNode *)malloc(sizeof(LLNode));
-    head = takeDataUnsortedLL(head);
-    sortTheLL(head);
-    // head = takeDataWithLinkedList(head);
-    printLLNode(head);
-    printf("Nodesize: %d BinaryHeight: %d\n", nodeSize, binaryTreeHeight(nodeSize));
-    // BTHead = createBTree(head);
+	LLNode *head;
+	BTNode *BTHead;
+	// head = (LLNode *)malloc(sizeof(LLNode));
+	head = takeDataUnsortedLL(head);
+	sortTheLL(head);
+	// head = takeDataWithLinkedList(head);
+	printLLNode(head);
+	printf("Nodesize: %d BinaryHeight: %d\n", nodeSize, BTNodeHeight(nodeSize));
+	BTHead = createBTree(head, nodeSize);
 }
 /*
 // !Error (Segmentation fault)
@@ -107,80 +102,113 @@ LLNode *takeDataWithLinkedList(LLNode *head)
 // *Works correct.
 LLNode *takeDataUnsortedLL(LLNode *head)
 {
-    int usr_id, usr_val;
-    scanf("%d %d", &usr_id, &usr_val);
-    LLNode *main = (LLNode *)malloc(sizeof(LLNode));
-    main->identity = usr_id;
-    main->value = usr_val;
-    head = main;
-    scanf("%d %d", &usr_id, &usr_val);
-    while (usr_id != -1 && usr_val != -1)
-    {
-        LLNode *body = (LLNode *)malloc(sizeof(LLNode));
-        body->identity = usr_id;
-        body->value = usr_val;
-        main->next = body;
-        main = main->next;
-        scanf("%d %d", &usr_id, &usr_val);
-    }
-    return head;
+	int usr_id, usr_val;
+	scanf("%d %d", &usr_id, &usr_val);
+	LLNode *main = (LLNode *)malloc(sizeof(LLNode));
+	main->identity = usr_id;
+	main->value = usr_val;
+	head = main;
+	scanf("%d %d", &usr_id, &usr_val);
+	while (usr_id != -1 && usr_val != -1)
+	{
+		LLNode *body = (LLNode *)malloc(sizeof(LLNode));
+		body->identity = usr_id;
+		body->value = usr_val;
+		main->next = body;
+		main = main->next;
+		scanf("%d %d", &usr_id, &usr_val);
+	}
+	return head;
 }
 
 // *Works correct.
 void printLLNode(LLNode *head)
 {
-    LLNode *ptr = head;
-    while (ptr != NULL)
-    {
-        nodeSize++;
-        printf("%d %d\n", ptr->identity, ptr->value);
-        ptr = ptr->next;
-    }
+	LLNode *ptr = head;
+	while (ptr != NULL)
+	{
+		nodeSize++;
+		printf("%d %d\n", ptr->identity, ptr->value);
+		ptr = ptr->next;
+	}
 }
 // *Works correct.
-int binaryTreeHeight(int BTSize)
+int BTNodeHeight(int BTSize)
 {
-    int n = 0;
-    while (pow(2, n)-1 < BTSize)
-        n++;
-    return n;
+	int n = 0;
+	while (pow(2, n) - 1 < BTSize)
+		n++;
+	return n;
 }
 
 //* Works correnct
 void sortTheLL(LLNode *head)
 {
-    int val, id;
-    LLNode *ptr1 = head;
-    while (ptr1 != NULL)
-    {
-        LLNode *ptr2 = head;
-        while (ptr2 != NULL)
-        {
-            if (ptr1->value < ptr2->value)
-            {
-                val = ptr1->value;
-                id = ptr1->identity;
-                ptr1->value = ptr2->value;
-                ptr1->identity = ptr2->identity;
-                ptr2->value = val;
-                ptr2->identity = id;
-            }
-            ptr2 = ptr2->next;
-        }
-        ptr1 = ptr1->next;
-    }
+	int val, id;
+	LLNode *ptr1 = head;
+	while (ptr1 != NULL)
+	{
+		LLNode *ptr2 = head;
+		while (ptr2 != NULL)
+		{
+			if (ptr1->value < ptr2->value)
+			{
+				val = ptr1->value;
+				id = ptr1->identity;
+				ptr1->value = ptr2->value;
+				ptr1->identity = ptr2->identity;
+				ptr2->value = val;
+				ptr2->identity = id;
+			}
+			ptr2 = ptr2->next;
+		}
+		ptr1 = ptr1->next;
+	}
 }
 
-BinaryTree *createBTree(LLNode *head)
+BTNode *createBTree(LLNode *head, int treeSize)
 {
-    BinaryTree *masterNode = (BinaryTree *)malloc(sizeof(BinaryTree));
+	printf("\nHere\n");
+	BTNode *masterNode = (BTNode *)malloc(sizeof(BTNode));
+	masterNode->left = (BTNode *)malloc(sizeof(BTNode));
+	masterNode->right = (BTNode *)malloc(sizeof(BTNode));
+	int binaryHeight = BTNodeHeight(treeSize);
 
-    LLNode BTArray[nodeSize];
-    for (size_t i = 0; i < nodeSize; i++)
-    {
-        BTArray[i] = *head;
-        head = head->next;
-    }
+	LLNode BTArray[nodeSize];
+	for (size_t i = 0; i < nodeSize; i++)
+	{
+		BTArray[i] = *head;
+		head = head->next;
+	}
 
-    return masterNode;
+	for (size_t i = 1; i < treeSize / 2; i++)
+	{
+		if ((i * 2) % 4 == 0)
+			continue;
+		//activeHeight = kaçıncı seviyede bulunduğunu tutar.
+		BTNode *temp = (BTNode *)malloc(sizeof(BTNode));
+
+		temp->left = (BTNode *)malloc(sizeof(BTNode));
+		temp->right = (BTNode *)malloc(sizeof(BTNode));
+
+		temp->identity = BTArray[2 * i - 1].identity;
+		temp->value = BTArray[2 * i - 1].value;
+		temp->left->identity = BTArray[2 * i - 2].identity;
+		temp->left->value = BTArray[2 * i - 2].value;
+		temp->right->identity = BTArray[2 * i].identity;
+		temp->right->value = BTArray[2 * i].value;
+
+		printf("Root ID/VAL: %d - %d \t Left ID/VAL: %d - %d \t Right ID/VAL: %d - %d\n",
+			   temp->identity, temp->value, temp->left->identity, temp->left->value,
+			   temp->right->identity, temp->right->value);
+	}
+
+	// masterNode->identity = BTArray[(int)pow(2, binaryHeight - 1)].identity;
+	// masterNode->value = BTArray[(int)pow(2, binaryHeight - 1)].value;
+	// masterNode->left = createBTree(masterNode->left, );
+	// masterNode->right = createBTree(masterNode->right, );
+
+	return masterNode;
 }
+
+//for içinde recursive fonksiyon olabilir atama yapmak için.
