10f68f2be8a8526c06145d122c8be23ec34aedcb
serqkan <serqkanseko@gmail.com>
2019-12-18 15:15:04 +0300

    ! ERROR

--- HOMEWORK-7/Homework.c
+++ HOMEWORK-7/Homework.c
@@ -35,14 +35,29 @@ void takeDataLL(LLNode **head);
 void printLL(LLNode *head);
 RBTreeNode *grandParent(RBTreeNode *head);
 void RBTreeInsert(RBTreeNode **RootOfTree, int data);
+void LeftRotate(RBTreeNode **HeadOfTree, RBTreeNode *temp);
+void RightRotate(RBTreeNode **HeadOfTree, RBTreeNode *temp);
+void fixInIter(RBTreeNode **HeadOfMaster, RBTreeNode *temp);
 
 // * Main functions
 
 int main()
 {
 	LLNode *LinkedList;
-	takeDataLL(&LinkedList);
-	printLL(LinkedList);
+	RBTreeNode *RootNode = (RBTreeNode *)malloc(sizeof(RBTreeNode));
+	// takeDataLL(&LinkedList);
+	// printLL(LinkedList);
+	int data;
+	scanf("%d", &data);
+	while (data != -1)
+	{
+		RBTreeInsert(&RootNode, data);
+		scanf("%d", &data);
+	}
+	printf("Head->left: %d and color->left: %c\n", RootNode->right->data, RootNode->right->Color);
+	printf("Head->left: %d and color->left: %c\n", RootNode->right->right->data, RootNode->right->right->Color);
+	printf("Head->left: %d and color->left: %c\n", RootNode->right->right->right->data, RootNode->right->right->right->Color);
+	printf("Head->left: %d and color->left: %c\n", RootNode->right->left->right->data, RootNode->right->left->right->Color);
 }
 
 void takeDataLL(LLNode **head)
@@ -90,7 +105,9 @@ void RBTreeInsert(RBTreeNode **RootOfTree, int data)
 		RBTreeNode *usefullNode = NULL;
 		RBTreeNode *temp = (RBTreeNode *)malloc(sizeof(RBTreeNode));
 		temp->data = data;
-		temp->left = temp->right = temp->parent == NULL;
+		temp->left = NULL;
+		temp->right = NULL;
+		temp->parent == NULL;
 		RBTreeNode *ptr = (*RootOfTree);
 		for (; ptr != NULL;)
 		{
@@ -116,7 +133,120 @@ void RBTreeInsert(RBTreeNode **RootOfTree, int data)
 		temp->Color = 'R';
 
 		//FixNode
+		fixInIter(RootOfTree, temp);
 	}
+}
+
+void LeftRotate(RBTreeNode **HeadOfTree, RBTreeNode *temp)
+{
+	if (!temp || !temp->right)
+		return;
+	RBTreeNode *tempRight = temp->right;
+	temp->right = tempRight->left;
+
+	if (temp->right != NULL)
+		temp->right->parent = temp;
+
+	tempRight->parent = temp->parent;
+
+	if (temp->parent == NULL)
+		(*HeadOfTree) = tempRight;
+
+	else if (temp == temp->parent->left)
+		temp->parent->left = tempRight;
+	else
+	{
+		temp->parent->right = tempRight;
+	}
+	tempRight->left = temp;
+
+	temp->parent = tempRight;
+}
+
+void RightRotate(RBTreeNode **HeadOfTree, RBTreeNode *temp)
+{
+	if (!temp || !temp->left)
+		return;
+	RBTreeNode *tempLeft = temp->left;
+	temp->left = tempLeft->right;
+	if (tempLeft->right != NULL)
+		tempLeft->right->parent = temp;
+	tempLeft->parent = temp->parent;
+
+	if (tempLeft->parent == NULL)
+		(*HeadOfTree) = tempLeft;
+
+	else if (temp == temp->parent->left)
+		temp->parent->left = tempLeft;
+
+	else
+	{
+		temp->parent->right = tempLeft;
+	}
+	tempLeft->right = temp;
+	temp->parent = tempLeft;
+}
+
+void fixInIter(RBTreeNode **HeadOfMaster, RBTreeNode *temp)
+{
+	while (temp != *HeadOfMaster && temp != (*HeadOfMaster)->left && temp != (*HeadOfMaster)->right && temp->parent->Color == 'R')
+	{
+		RBTreeNode *gecici;
+
+		if (temp->parent && temp->parent->parent && temp->parent == temp->parent->parent->left)
+			gecici = temp->parent->parent->right;
+		else
+			gecici = temp->parent->parent->left;
+
+		if (!gecici)
+			temp = temp->parent->parent;
+		else if (gecici->Color == 'R')
+		{
+			gecici->Color = 'B';
+			temp->parent->Color = 'B';
+			temp->parent->parent->Color = 'R';
+			temp = temp->parent->parent;
+		}
+
+		else
+		{
+			if (temp->parent == temp->parent->parent->left && temp == temp->parent->left)
+			{
+				char tempParentColor = temp->parent->Color;
+				temp->parent->Color = temp->parent->parent->Color;
+				temp->parent->parent->Color = tempParentColor;
+				RightRotate(HeadOfMaster, temp->parent->parent);
+			}
+
+			if (temp->parent && temp->parent->parent && temp->parent == temp->parent->parent->left && temp == temp->parent->left)
+			{
+				char tempColor = temp->Color;
+				temp->Color = temp->parent->parent->Color;
+				temp->parent->parent->Color = tempColor;
+				LeftRotate(HeadOfMaster, temp->parent);
+				RightRotate(HeadOfMaster, temp->parent->parent);
+			}
+
+			if (temp->parent && temp->parent->parent && temp->parent == temp->parent->parent->right && temp == temp->parent->right)
+			{
+				char tempParColor = temp->parent->Color;
+				temp->parent->Color = temp->parent->parent->Color;
+				temp->parent->parent->Color = tempParColor;
+				LeftRotate(HeadOfMaster, temp->parent->parent);
+			}
+
+			if (temp->parent && temp->parent->parent && temp->parent == temp->parent->parent->right && temp == temp->parent->left)
+			{
+				char tempColor = temp->Color;
+				temp->Color = temp->parent->parent->Color;
+				temp->parent->parent->Color = tempColor;
+				RightRotate(HeadOfMaster, temp->parent);
+				LeftRotate(HeadOfMaster, temp->parent->parent);
+			}
+		}
+	}
+
+	(*HeadOfMaster)->Color = 'B';
 }
 
 // ! https://gist.github.com/VictorGarritano/5f894be162d39e9bdd5c
